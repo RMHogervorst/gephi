f24d8af73444ab95993312a74b2c25d8398d5764
Roel Hogervorst <hogervorst.rm@gmail.com>
2018-04-08 09:18:07 +0200

    writes igraph/tidygraph to 2 csv files
    basic functionality fixed. writes to edges
    and nodes csv

--- tests/testthat/test_outputfile.R
+++ tests/testthat/test_outputfile.R
@@ -4,14 +4,13 @@ context("testing output of csv files")
 ## create the file we need
 
 test_that("edgefile has correct columns", {
-    # create file
-    testdir <- tempdir()
-    # create edgefile
 
+    # create edgefile
     edgefilelocation <- "edgefile.csv"
-    #
-    write_edgefile(x = testingedgeframe, path = edgefilelocation)
+    write_edgefile(x = graphexample, path = edgefilelocation)
+    # read the file in
     read_edgefile <- read.csv(edgefilelocation)
+    file.remove(edgefilelocation)
     # now the testing is started
     expect_match(names(read_edgefile)[1:2,], c("Target", "Source"))
 })
--- todo.md
+++ todo.md
@@ -1,6 +1,7 @@
 *start with writing tests than create the functions*
 
-- [] take igraph/tidygraph object and write to node and edges csv
+- [x] take igraph/tidygraph object and write to node and edges csv
 - [] read in edge.csv as igraph / tidygraph object
-- [] take any dataframe and convert to edgescsv (Target, Source, *Weight, *Type)
+- [x] take any dataframe and convert to edgescsv (Target, Source, *Weight, *Type)
 - [] take any dataframe and convert to nodescsv (Id, Label)
+- [] create example testfiles

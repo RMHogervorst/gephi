00e96f0296a59c95978b3c95276174bab7b1a21b
Roel Hogervorst <hogervorst.rm@gmail.com>
2018-06-01 22:33:58 +0200

    contains pkgdown update

---
+++ R/utils.R
@@ -0,0 +1,16 @@
+#' Write to csv
+#'
+#' This function will use the readr version of write_csv
+#' if it can be found, and will fall back to write.csv
+#' from base if not. This allows me to keep the number
+#' of dependencies low.
+#' @importFrom utils write.csv
+#' @noRd
+write_safe <- function(x, path, na = ""){
+    if(requireNamespace("readr", quietly = TRUE)){
+        readr::write_csv(x = x, path = path, na = na)
+    }else{
+        write.csv(x = x, file = path, na = na)
+    }
+}
+
--- tests/testthat/test_outputfile.R
+++ tests/testthat/test_outputfile.R
@@ -10,7 +10,20 @@ test_that("edgefile has correct columns", {
     gephi_write_edges(graphexample, path = edgefilelocation)
     # read the file in
     read_edgefile <- read.csv(edgefilelocation)
-    file.remove(edgefilelocation)
+
     # now the testing is started
     expect_true(all(names(read_edgefile)[1:2] == c( "Source","Target")))
 })
+
+context("testing convertion to igraph")
+
+test_that("correct igraph object is created", {
+    # read it in.
+    graph_csv <- gephi_read_edges_csv(edgefilelocation, source = "Source", target = "Target")
+    expect_true(igraph::identical_graphs(gephi::graphexample, graph_csv))
+
+    rm(graph_csv)
+})
+
+
+file.remove(edgefilelocation)
